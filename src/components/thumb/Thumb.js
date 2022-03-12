69bd8db9f217552afa235ca7b61fc3925b3d50a0
rayanadir <rayan.dahmena@gmail.com>
2022-03-12 16:11:51 +0100

    création composant Thumb

---
+++ src/components/thumb/Thumb.js
@@ -0,0 +1,12 @@
+import '../thumb/Thumb.css'
+
+const Thumb = () =>{
+    return (
+        <div className="thumb">
+            <img src="./kasa_logo.jpg" alt="logo" className="thumbImg"/>
+            <p className="thumbTitle">Nom de la location</p>
+        </div>
+    )
+}
+
+export default Thumb
