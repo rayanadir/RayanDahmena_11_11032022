ce026352a83102cc202a1e686e532f3c05f79940
rayanadir <rayan.dahmena@gmail.com>
2022-03-16 14:08:55 +0100

    ajout proptypes composant carrousel

--- src/components/carrousel/Carrousel.jsx
+++ src/components/carrousel/Carrousel.jsx
@@ -2,6 +2,7 @@ import '../carrousel/Carrousel.scss'
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons'
+import PropTypes from 'prop-types';
 
 const Carrousel = (props) => {
     return(
@@ -12,6 +13,11 @@ const Carrousel = (props) => {
             <p className={`carrousel__index__${props.size}`}>1/4</p>
         </div>
     )
+}
+
+Carrousel.propTypes={
+    size:PropTypes.string,
+    image:PropTypes.string,
 }
 
 export default Carrousel
