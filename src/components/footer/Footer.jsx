fc025a19dde1abfdd37a7f7b1c0ec74af4275324
rayanadir <rayan.dahmena@gmail.com>
2022-03-19 18:18:33 +0100

    changement composant footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,12 +1,11 @@
 import './Footer.scss';
-import image from '../../assets/logo_footer.png'
+import logo from '../../assets/logo_footer.png'
 
 const Footer = () => {
     const year= new Date().getFullYear();
-    console.log(image);
     return(
         <div className="footer">
-            <img src={image} alt="footer_logo" className="footer__logo" />
+            <img src={logo} alt="footer_logo" className="footer__logo" />
             <p className='footer__copyrights'>© {year} Kasa. All rights reserved</p>
         </div>
     )
