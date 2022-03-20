e187bc62646c172286f5670161b8ec74235d33bb
rayanadir <rayan.dahmena@gmail.com>
2022-03-20 20:14:04 +0100

    mise a jour composant Carrousel

--- src/components/carrousel/Carrousel.jsx
+++ src/components/carrousel/Carrousel.jsx
@@ -5,18 +5,23 @@ import { faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons
 import PropTypes from 'prop-types';
 
 const Carrousel = (props) => {
-    return(
+    if(props.type==="about"){
+        return (<img src={props.image} className="about_image"  alt="about" />)
+    }else if(props.type==="cover"){
+        return(
         <div className="carrousel">
             <FontAwesomeIcon icon={faChevronLeft} className="carrousel__chevronLeft chevron" />
             <img src={props.image} className="carrousel__image" alt="cover"/>
             <FontAwesomeIcon icon={faChevronRight} className="carrousel__chevronRight chevron" />
             <p className="carrousel__index">1/4</p>
         </div>
-    )
+        )
+    }
 }
 
 Carrousel.propTypes={
     image:PropTypes.string,
+    type:PropTypes.string,
 }
 
 export default Carrousel
