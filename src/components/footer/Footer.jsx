18eaf3be3981277c976c64133e68e1692be43224
rayanadir <rayan.dahmena@gmail.com>
2022-03-20 20:15:18 +0100

    mise a jour composant Footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -4,10 +4,10 @@ import logo from '../../assets/logo_footer.png'
 const Footer = () => {
     const year= new Date().getFullYear();
     return(
-        <div className="footer">
+        <footer className="footer">
             <img src={logo} alt="footer_logo" className="footer__logo" />
             <p className='footer__copyrights'>© {year} Kasa. All rights reserved</p>
-        </div>
+        </footer>
     )
 }
 
