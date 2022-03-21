dfce90b76e0bc92e46afdedb5531f198ff64b58b
rayanadir <rayan.dahmena@gmail.com>
2022-03-21 21:54:57 +0100

    ajout page Error404

---
+++ src/views/Error404/Error404.jsx
@@ -0,0 +1,16 @@
+import './Error404.scss'
+import Footer from '../../components/footer/Footer'
+import Header from '../../components/header/Header'
+import Error from '../../components/error/Error';
+
+const Error404 = () => {
+    return(
+        <div>
+            <Header />
+            <Error />
+            <Footer />
+        </div>
+    )
+}
+
+export default Error404
