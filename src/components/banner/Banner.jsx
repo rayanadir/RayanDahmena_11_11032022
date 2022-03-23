8077878bca0657eebbc32e8f645d474dd531bbda
rayanadir <rayan.dahmena@gmail.com>
2022-03-23 21:16:39 +0100

    mise à jour bannière

--- src/components/banner/Banner.jsx
+++ src/components/banner/Banner.jsx
@@ -2,11 +2,25 @@ import '../banner/Banner.scss';
 import PropTypes from 'prop-types';
 
 const Banner = (props) => {
-    return (<img src={props.image} className="banner"  alt="about" />)
+    if(props.type==="about"){
+        return (
+            <section className="banner">
+                <img src={props.image} className="banner__image"  alt="about" />
+            </section>    
+            )
+    }else if(props.type==="home"){
+        return (
+            <section className="banner">
+                <img src={props.image} className="banner__image home"  alt="about" />
+                <p className="banner__text">Chez vous, partout et ailleurs</p>
+            </section>    
+            )
+    }
 }
 
 Banner.propTypes={
-    image: PropTypes.string
+    image: PropTypes.string,
+    type: PropTypes.string,
 }
 
 export default Banner
