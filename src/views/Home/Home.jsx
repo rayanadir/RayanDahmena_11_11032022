c9bbca6c4d91da23b00602e215685785e4e39faf
rayanadir <rayan.dahmena@gmail.com>
2022-03-23 21:19:59 +0100

    ajout composants Thumb dans page Home

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -1,18 +1,26 @@
-import '../Home/Home.scss'
+import '../Home/Home.scss';
 import React from 'react';
 import Header from '../../components/header/Header';
 import Footer from '../../components/footer/Footer';
+import Banner from '../../components/banner/Banner';
+import background from '../../assets/background_home.png';
+import Gallery from '../../components/gallery/Gallery';
+import PropTypes from 'prop-types';
 
-const Home = () => {
+const Home = (props) => {
     return(
         <div>
             <Header />
-            
+            <Banner image={background} type="home" />
+            <Gallery data={props.data} />
             <Footer />
-        </div>
-            
-        
+        </div>    
     )
+}
+
+Home.propTypes={
+    image:PropTypes.string,
+    data:PropTypes.array,
 }
 
 export default Home
