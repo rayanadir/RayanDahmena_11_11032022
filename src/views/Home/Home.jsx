223a119ef16e782d15407a8c4020373bb853e5f3
rayanadir <rayan.dahmena@gmail.com>
2022-03-24 18:14:26 +0100

    mise a jour page Home

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -1,7 +1,5 @@
 import '../Home/Home.scss';
 import React from 'react';
-import Header from '../../components/header/Header';
-import Footer from '../../components/footer/Footer';
 import Banner from '../../components/banner/Banner';
 import background from '../../assets/background_home.png';
 import Gallery from '../../components/gallery/Gallery';
@@ -9,12 +7,10 @@ import PropTypes from 'prop-types';
 
 const Home = (props) => {
     return(
-        <div>
-            <Header />
+        <main>
             <Banner image={background} type="home" />
             <Gallery data={props.data} />
-            <Footer />
-        </div>    
+        </main>
     )
 }
 
