55c39d0231cc11d4a1eb173de8c7806dc913175d
rayanadir <rayan.dahmena@gmail.com>
2022-03-25 20:51:58 +0100

    mise a jour Carrousel

--- src/components/carrousel/Carrousel.jsx
+++ src/components/carrousel/Carrousel.jsx
@@ -6,12 +6,12 @@ import PropTypes from 'prop-types';
 
 const Carrousel = (props) => {
         return(
-        <div className="carrousel">
+        <section className="carrousel">
             <FontAwesomeIcon icon={faChevronLeft} className="carrousel__chevronLeft chevron" />
             <img src={props.image} className="carrousel__image" alt="cover"/>
             <FontAwesomeIcon icon={faChevronRight} className="carrousel__chevronRight chevron" />
             <p className="carrousel__index">1/4</p>
-        </div>
+        </section>
         )
 }
 
