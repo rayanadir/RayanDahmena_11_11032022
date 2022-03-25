4738c6e70298cf97fdaade6cf95d2caa854ae9c4
rayanadir <rayan.dahmena@gmail.com>
2022-03-25 20:54:12 +0100

    mise a jour page About

--- src/views/About/About.jsx
+++ src/views/About/About.jsx
@@ -1,7 +1,5 @@
 import '../Home/Home.scss'
 import React from 'react';
-import Header from '../../components/header/Header';
-import Footer from '../../components/footer/Footer';
 import AboutDropdowns from '../../components/aboutDropdowns/AboutDropdowns';
 import background from '../../assets/background_about.png'
 import Banner from '../../components/banner/Banner';
