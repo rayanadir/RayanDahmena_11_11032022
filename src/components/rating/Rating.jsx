c10b57a12988a034e2c83735f891f1877f59a1d5
rayanadir <rayan.dahmena@gmail.com>
2022-03-25 20:53:02 +0100

    ajout composant Rating

---
+++ src/components/rating/Rating.jsx
@@ -0,0 +1,21 @@
+import '../rating/Rating.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faStar } from '@fortawesome/free-solid-svg-icons';
+import React from 'react';
+
+const Rating = (props) => {
+    const rate=parseInt(props.rate);
+    const difference=5-rate;
+    return (
+        <React.StrictMode>
+            {Array.from(Array(rate), (e,i) =>{
+                return <FontAwesomeIcon icon={faStar} className='star filled' key={i}/>;
+            })}
+            {Array.from(Array(difference), (e,i) =>{
+                return <FontAwesomeIcon icon={faStar} className='star empty' key={i} />;
+            })}
+        </React.StrictMode>
+    )
+}
+
+export default Rating
