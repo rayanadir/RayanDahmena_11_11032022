0e90be9c067c6e866971f3927e21049d5c381e3f
rayanadir <rayan.dahmena@gmail.com>
2022-03-27 19:46:02 +0200

    composant Carrousel fonctionnel

--- src/components/carrousel/Carrousel.jsx
+++ src/components/carrousel/Carrousel.jsx
@@ -1,16 +1,26 @@
 import '../carrousel/Carrousel.scss'
-import React from 'react'
+import React, { useState } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons'
 import PropTypes from 'prop-types';
 
 const Carrousel = (props) => {
+    const [activeIndex, setActiveIndex]=useState(0);
+    const pictures=props.pictures;
+
+        const previous=()=>{
+            setActiveIndex((a) => (a - 1 < 0 ? pictures.length - 1 : a - 1))
+        }
+        const next=()=>{
+            setActiveIndex((a) => (a + 1 > pictures.length - 1 ? 0 : a + 1));
+        }
+
         return(
         <section className="carrousel">
-            <FontAwesomeIcon icon={faChevronLeft} className="carrousel__chevronLeft chevron" />
-            <img src={props.image} className="carrousel__image" alt="cover"/>
-            <FontAwesomeIcon icon={faChevronRight} className="carrousel__chevronRight chevron" />
-            <p className="carrousel__index">1/4</p>
+            <FontAwesomeIcon icon={faChevronLeft} className="carrousel__chevronLeft chevron" onClick={previous} />
+             <img src={pictures[activeIndex]} className="carrousel__image" alt="cover"/>
+            <FontAwesomeIcon icon={faChevronRight} className="carrousel__chevronRight chevron" onClick={next} />
+            <p className="carrousel__index">{activeIndex+1}/{props.pictures.length}</p>
         </section>
         )
 }
