0303d899fbaf2ac993ebb481f7852a9e2d7ba15c
rayanadir <rayan.dahmena@gmail.com>
2022-03-28 21:53:23 +0200

    mise à jour Gallery

--- src/components/gallery/Gallery.jsx
+++ src/components/gallery/Gallery.jsx
@@ -1,12 +1,15 @@
 import '../gallery/Gallery.scss'
 import PropTypes from 'prop-types';
 import Thumb from '../thumb/Thumb';
+import { Link } from 'react-router-dom';
 
 const Gallery = (props) => {
     return (
         <section className="gallery">
             {props.data.map((data)=>(
-                <Thumb cover={data.cover} name={data.title} key={data.id} />
+                <Link to={`/house/${data.id}`} key={data.id}>
+                    <Thumb cover={data.cover} name={data.title} key={data.id} />
+                </Link>
             ))}
         </section>
     )
