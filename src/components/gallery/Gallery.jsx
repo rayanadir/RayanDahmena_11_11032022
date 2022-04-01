98343e0c53aef11cc3fad64c8ba325d047fda658
rayanadir <rayan.dahmena@gmail.com>
2022-04-01 16:04:06 +0200

    ajout commentaires Gallery

--- src/components/gallery/Gallery.jsx
+++ src/components/gallery/Gallery.jsx
@@ -2,12 +2,18 @@ import '../gallery/Gallery.scss'
 import PropTypes from 'prop-types';
 import Thumb from '../thumb/Thumb';
 import { Link } from 'react-router-dom';
+import setUnderline from '../../app/active_element';
 
+/**
+ * 
+ * @param {*} props création composant Gallery 
+ * @returns galerie comportant des miniatures
+ */
 const Gallery = (props) => {
     return (
         <section className="gallery">
             {props.data.map((data)=>(
-                <Link to={`/house/${data.id}`} key={data.id}>
+                <Link to={`/house/${data.id}`} key={data.id} className="link" onClick={()=>setUnderline('house')}>
                     <Thumb cover={data.cover} name={data.title} key={data.id} />
                 </Link>
             ))}
