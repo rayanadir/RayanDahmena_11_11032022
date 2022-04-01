369a90ad58bef8d8caa38e743ba7a73f3924b202
rayanadir <rayan.dahmena@gmail.com>
2022-04-01 16:07:05 +0200

    ajout commentaires House

--- src/views/House/House.jsx
+++ src/views/House/House.jsx
@@ -4,34 +4,43 @@ import Carrousel from '../../components/carrousel/Carrousel'
 import Tag from '../../components/tag/Tag';
 import Rating from '../../components/rating/Rating';
 import Dropdown from '../../components/dropdown/Dropdown';
+import { useParams } from 'react-router-dom'
+import { data } from '../../data/logements';
 
-const House= (props) => {
+/**
+ * 
+ * @returns page d'appartement
+ */
+const House= () => {
+    window.scrollTo(0,0)
+    let {id} = useParams();
+    const house=data.filter(house=>house.id===id)
     return(
         <main>
-            <Carrousel  image={props.data.pictures[0]} key={props.data.pictures[0]} pictures={props.data.pictures} />
+            <Carrousel image={house[0].pictures[0]} key={house[0].pictures[0]} pictures={house[0].pictures} />
             <section className='infos'>
                 <div className="infos__title_localisation_tags">
                     <div className="infos__title_localisation">
-                        <h2 className="infos__title">{props.data.title}</h2>
-                        <h5 className="infos__localisation">{props.data.location}</h5>
+                        <h2 className="infos__title">{house[0].title}</h2>
+                        <h5 className="infos__localisation">{house[0].location}</h5>
                     </div>
                     <div className="infos__tags">
-                        {props.data.tags.map((tag)=>( <Tag name={tag} key={tag} /> ))}
+                        {house[0].tags.map((tag)=>( <Tag name={tag} key={tag} /> ))}
                     </div>
                 </div>
                 <div className="infos__host_rate">
                     <div className="infos__host">
-                        <p className="infos__hostName">{props.data.host.name}</p>
-                        <img className='infos__hostPicture' src={props.data.host.picture} alt={props.data.host.name}/>
+                        <p className="infos__hostName">{house[0].host.name}</p>
+                        <img className='infos__hostPicture' src={house[0].host.picture} alt={house[0].host.name}/>
                     </div>
                     <div className="infos__rate" id='rating'>
-                        <Rating rate={props.data.rating} />
+                        <Rating rate={house[0].rating} />
                     </div>
                 </div>
             </section>
             <section className='dropdowns'>
-                <Dropdown type='equipements_description' name='Description' content={props.data.description} />
-                <Dropdown type='equipements_description' name='Equipements' content={props.data.equipments}/>
+                <Dropdown type='equipements_description' name='Description' content={house[0].description} />
+                <Dropdown type='equipements_description' name='Equipements' content={house[0].equipments}/>
             </section>
         </main>
     )
