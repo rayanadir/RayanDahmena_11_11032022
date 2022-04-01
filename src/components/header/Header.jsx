053576c808f0047b3e9412f88c0326940a374bf3
rayanadir <rayan.dahmena@gmail.com>
2022-04-01 16:04:26 +0200

    ajout commentaires Header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,19 +1,26 @@
 import '../header/Header.scss';
 import logo from '../../assets/logo_header.png';
 import { Link } from 'react-router-dom';
+import setUnderline from '../../app/active_element';
 
+/**
+ * 
+ * @returns création composant Header
+ */
 const Header = () => {
     return(
             <header className='header'>
-                <Link to='/'>
+                <Link to='/' onClick={() => {setUnderline('home')}}>
                     <img src={logo} className='header__logo' alt='header' />
                 </Link>
                 <nav className="header__nav">
-                    <Link to='/' className='header__element'>Accueil</Link>
-                    <Link to='/about' className='header__element'>A propos</Link>
+                    <Link to='/' className='header__element underline' id='home' onClick={() => {setUnderline('home')}}>Accueil</Link>
+                    <Link to='/about' className='header__element' id='about' onClick={()=>{setUnderline('about')}}>A propos</Link>
                 </nav>
             </header>
     )
 }
+
+
 
 export default Header
